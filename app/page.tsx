c48536b888924433e369ee98e36f3540835fa981
Franck Martin <f.martin@astonitf.com>
2023-12-20 10:31:09 +0100

    Some style

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,12 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <h1 className="text-7xl text-center text-amber-600">Chargeur Induction Pliable</h1>
-        <h2 className="text-6xl text-center text-amber-500">Chargeur sans Fil 3 en 1</h2>
+        <p className="underline">Je ne peux pas te présenter en réel le cadeau du à un retard de colis. Mais permets moi de te l'offrir numériquement pour le moment.</p>
+        <br />
+        <br />
+
+        <h1 className="text-3xl text-amber-600">Chargeur Induction Pliable</h1>
+        <h2 className="text-2xl text-amber-500">Chargeur sans Fil 3 en 1</h2>
         <br />
         <img src="/1.jpg" />
         <br />
