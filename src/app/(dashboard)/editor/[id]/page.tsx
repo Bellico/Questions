3dae14745b4e3fe56c3fbd068f8231e53b764fdf
Franck Martin <f.martin@astonitf.com>
2024-03-27 18:18:38 +0100

    Use maxDuration

--- src/app/(dashboard)/editor/[id]/page.tsx
+++ src/app/(dashboard)/editor/[id]/page.tsx
@@ -4,6 +4,8 @@ import { QuestionsEditor } from '@/components/editor/questions-editor'
 import { auth } from '@/lib/auth'
 import { notFound } from 'next/navigation'
 
+export const maxDuration = 10
+
 export default async function EditorPage({
   params,
   searchParams,
