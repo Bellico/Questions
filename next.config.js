a1f8814cb3b00cfefeb06fe34ce7715f6a15800f
Franck Martin <f.martin@astonitf.com>
2024-04-25 16:56:16 +0200

    clean

--- next.config.js
+++ next.config.js
@@ -1,24 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: '/:path*',
-        headers: [
-          // { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: 'https://blog.logrocket.com' }, // replace this your actual origin
-          { key: 'Access-Control-Allow-Methods', value: 'GET,POST,OPTIONS' },
-          // { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value: 'Content-Type, Authorization',
-          },
-        ]
-      }
-    ]
-  }
+  output: 'standalone'
 }
 
 module.exports = nextConfig
